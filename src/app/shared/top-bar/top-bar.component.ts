1c870047580506b742864125e22b5a928375ff9d
Andre <andre.ncardoso@hotmail.com>
2024-03-19 14:54:41 -0300

    feat: create svg icons

--- src/app/shared/top-bar/top-bar.component.ts
+++ src/app/shared/top-bar/top-bar.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import {ButtonComponent} from "../button/button.component";
+import {LogoComponent} from "../../assets/svg/logo/logo.component";
+import {InstagramComponent} from "../../assets/svg/instagram/instagram.component";
+import {YoutubeComponent} from "../../assets/svg/youtube/youtube.component";
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
   imports: [
-    ButtonComponent
+    ButtonComponent,
+    LogoComponent,
+    InstagramComponent,
+    YoutubeComponent
   ],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss'
