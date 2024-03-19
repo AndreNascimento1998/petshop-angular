9dc2dcdf745b7d9cabd492ae03d68e94912c2a63
Andre <andre.ncardoso@hotmail.com>
2024-03-19 16:20:22 -0300

    style: create animation color change

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,14 +1,18 @@
-import {Component, Output, EventEmitter} from '@angular/core';
+import {Component, Output, EventEmitter, Input} from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
   @Output() clickEvent = new EventEmitter<void>()
+  @Input() animate: boolean = false
   onClick() {
     this.clickEvent.emit()
   }
